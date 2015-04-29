d5772a7eb361794568fe9672cde1cb20cc412af9
4nonhumans <aradhakrishnan.sh@mysummitps.org>
2015-04-29 17:10:39 +0000

    first commit

---
+++ fire.js
@@ -0,0 +1,77 @@
+//Variables for our game
+var playerX = 0;
+var playerY = 0;
+var arrows = [];
+var points = 0;
+var inGame=false;
+var character = {
+  Name: "Mr. Fire",
+  Height: "3'1",
+  Gender: "Fire",
+  Color: "Red",
+};
+//StartGame Function
+document.getElementById("StartGame").addEventListener("click",startGame);
+document.onkeydown = checkKey;
+function startGame(){
+    inGame=true;
+    playerX = 0;
+    playerY = -7;
+    arrows = [];
+    points = 0;
+  window.requestAnimationFrame(update);
+}
+//Movement of the player
+function checkKey(e) {
+    e = e || window.event;
+    //right arrow key
+    if (e.keyCode == '39'&&playerX!=7) {
+       
+        playerX++;
+    }
+    //left arrow key
+    if (e.keyCode == '37'&&playerX!=-7) {
+       
+        playerX--;
+    }
+}
+//Random raindrops falling vertically
+function randomArrow(){
+    arrows.push([Math.floor(Math.random()*15)-7,7]);
+}
+//Update on screen
+function update(){
+  if(!inGame)return;
+    if(Math.random()>0.7)randomArrow();
+    //console.log(arrowarrrows);
+    var pos1 = -7;
+var pos2 = -7;
+for(i=0;i<225;i++){
+    document.getElementById("spot"+pos1+pos2).innerHTML = "";
+    pos1++;
+    if(pos1==8){
+    pos1=-7;
+    pos2++;
+      if(pos2==8)pos2=0;
+    }
+}
+  //Runs arrows on the screen for each arrow
+  for(i=0;i<arrows.length;i++){
+    //Prints the arrows on the screen
+      document.getElementById("spot"+Math.floor(arrows[i][0])+Math.floor(arrows[i][1])).innerHTML="<div style='color:blue'><img class='arrow' src='http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/blue-jelly-icons-natural-wonders/050300-blue-jelly-icon-natural-wonders-raindrop1-sc52.png'/><div>";
+      //Moves arrow down updates Y position
+      arrows[i][1]-=0.3;
+      if(arrows[i][1]<-7){
+        arrows.splice(i,1);
+        points++;
+      }
+      //Updates points
+      document.getElementById("PointSystem").innerHTML=points;
+      //Checks if raindrops hits player then ends game
+        if (playerX == Math.round(arrows[i][0]) && playerY == Math.round(arrows[i][1])){
+       inGame=false;
+    }
+        document.getElementById("spot" + playerX + playerY).innerHTML = "<div style='color:gold'><img style='width:74%' src='http://socialeyesnyc.com/wp-content/uploads/2012/04/hot-flame.png'/></div>";
+    }
+window.requestAnimationFrame(update);
+}
---
+++ hardware.js
@@ -0,0 +1,34 @@
+//CPU Cooler innerHTML <------------
+function myFunction() {
+    document.getElementById("hardware").innerHTML = "Since some computers run more than 18 hours a day, the processor generates a lot of heat which is caused by the internal energy of the computer. Improper maintance of the parts of the computer could reduce it's life span or could increase damage. CPU fans cool up parts of a computer and prevent them from overheating. Some of these parts are: circuits, motherboard, RAM, or harddrives. CPU fans are sold in many different sizes and brands and are usually covered with aluminum or copper to dispel any hot air. CPU coolers are one of the most important part needed for your computer. <br><br><br><img src='http://imagescdn.tweaktown.com/pr/5/0/5035_5_arctic_announces_the_freezer_13_pro_cpu_cooler.png' width = '200px'><img s";
+}
+
+//Motherboard innerHTML <---------------
+function myFunction1() {
+    document.getElementById("hardware").innerHTML = "A motherboard is a printed circuit board containing the principal parts of a computer. The board also has many connections in which other boards/wites could be insterted into. The motherboard serves to connect all the parts of a computer together. The CPU, memory, hard drives, optical drives, video card, sound card and other ports and expansion cards all connect to the motherboard directly or via cables. The motherboard connects to all the other parts of the computer to communicate with them. These parts include the RAM, Hard Drive, CPU and etc.. The motherboard is essential for your computer to work.<br><br><br><img src='http://i847.photobucket.com/albums/ab32/flint117/motherboard.png:original' width = '330px'>";
+}
+
+//RAM innerHTML <-----------------
+function myFunction2() {
+    document.getElementById("hardware").innerHTML = "RAM, short for Random Access Memories is used for computer storage that can be accessed randomly. The RAM is stored by DATA that is only accessible by the CPU. Ram is stored as data in bytes.<br><br><br><img src='https://tito0809.files.wordpress.com/2011/01/pc-ram.png' width = '300px'>";
+}
+
+//Graphics Card innerHTML <------------------
+function myFunction3() {
+    document.getElementById("hardware").innerHTML = "The graphics components are the part of your computer that control and enhance how graphics (pictures, videos, programs, animation, 3D) are displayed on your computer screen. A graphics card takes data from the CPU and converts it into an image.<br><br><b><p>⚡ Different brands of Graphics Cards ⚡</p></b><li>Nvidia</li><li>ASUS</li><li>Sapphire</li><li>MSI</li><br><br><br><img src='http://www.geniusdv.com/news_and_tutorials/2009/09/13/Quadro_fx_4800_mac.png' width = '300px'>";
+}
+
+//External Card innerHTML <------------------
+function myFunction4() {
+    document.getElementById("hardware").innerHTML = "External parts of your computer include a mouse and a keyboard. A keyboard is a panel of keys that operate a computer or typewriter. Many mice have two buttons and a scroll wheel. A computer mouse is an input device that is most often used with a personal computer. Moving a mouse along a flat surface can move the on-screen cursor to different items on the screen.<br><br><div='keyboardtable'><table align= 'center'><p class='table-title'><b>Types of Keyboards</b></p><tr><td><b>Gaming Keyboards ⇩</b></td><td><b>Work Keyboards ⇩</b></td></tr><tr><td>Razer BlackWidow</td><td>Logitech MK270</td></tr><tr><td>Corsair K70</td><td>AZIO PRISM KB507</td></tr><tr><td>CM Storm Devastator</td><td>Microsoft Sculpt</td></tr><tr><td>Logitech G710</td><td>Apple Keyboard</td></tr></table></div><br><br><br><img src='http://www.umpcportal.com/gallery/d/41425-1/touchpad-accessories-keyboard.png' width = '300px'><img src='http://www.burrows.co.uk/proofs/catalyst/assets/images/publishing/mouse.png' width = '200px'/>";
+}
+
+//CPU Card innerHTML <------------------
+function myFunction5() {
+    document.getElementById("hardware").innerHTML = "CPU also known as the Central Processing Unit processes instruction that it gathers from decoding the programs and other files. A CPU has 4 primary functions: fetch, decode, execute, and writeback. <b>What is a CPU made of?</b> A CPU is a complex integrated circuit made of silicon. A modern CPU can have billions of microscopic transistors mounted on it. The CPU is typically enclosed by a metallic heat spreader which allows for better heat dissipation.<br><br><img src='http://fc08.deviantart.net/fs70/f/2011/043/0/4/core_i7_sandy_bridge_cpu_by_climber07-d39emb2.png' width = '200px'/>";
+}
+
+//Hard Drive Card innerHTML <------------------
+function myFunction6() {
+    document.getElementById("hardware").innerHTML = "A Hard Drive is a data storage device used for storing and retrieving digital information using rapidly rotating disks coated with magnetic material. Everything you keep on a computer is stored in the hard drive. The hard drive is actually a few circular disks stacked one on top of the other. A little arm moves over the disks and writes things to these circular platters, and reads them. When you save a file or create a new folder, think of these circular platters being written to and you'll have a basic idea of just what your hard drive is. A hard drive is essential for your computer.<br><br><img src='http://icons.iconarchive.com/icons/harwen/simple/256/Hard-Drive-icon.png' width = '200px'/>";
+}
